db14c03947ae35a8b6767949a367c19761181150
Seb4sti4n27 <27sebastian4@gmail.com>
2023-06-09 02:29:46 -0500

    ty

--- Ejer-soap-dias-master/dsw1-J19/cliente-soap-dias/target/generated-sources/xjc/com/example/consumingwebservice/wsdl/ObjectFactory.java
+++ Ejer-soap-dias-master/dsw1-J19/cliente-soap-dias/target/generated-sources/xjc/com/example/consumingwebservice/wsdl/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.06.09 a las 02:10:31 AM PET 
+// Generado el: 2023.06.09 a las 02:19:24 AM PET 
 //
 
 
